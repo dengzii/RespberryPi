9d8602abcdd65decff5a88df0c442ed08fb8e547
DENUA <denua@qq.com>
2018-06-18 15:57:03 +0800

    add get_up_time

--- LCD1602_IIC/host_status.py
+++ LCD1602_IIC/host_status.py
@@ -1,6 +1,11 @@
 import socket
 import fcntl
 import struct
+import psutil
+import time
+import datetime
+
+BOOT_TIME = psutil.boot_time()
 
 def get_ip_address(ifname):
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -12,3 +17,8 @@ def get_ip_address(ifname):
 		)[20:24])
 	except:
 		return "None"
+
+def get_up_time():
+
+    up = time.time() - BOOT_TIME
+    return datetime.datetime.fromtimestamp(up).strftime('%d-%H%M%S')
